ec72fc206fe45ce22f172ed8db5ebb2d73085d0e
prakash.prasad <infymee@gmail.com>
2025-09-16 10:19:27 +0545

    changes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
-// import axios from "axios";
-const axios = require("axios");
+import axios from "axios";
+// const axios = require("axios");
 
 // Export the configuration as default export (ESM style)
 export default {
